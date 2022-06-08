8a3c691e5ae2dfcf1d6abbd9777a2dc0f6a21a05
Karuppasamych <106671462+Karuppasamych@users.noreply.github.com>
2022-06-08 11:55:36 +0530

    Add files via upload

---
+++ number-to-words.js
@@ -0,0 +1,56 @@
+let intnumber = prompt('Enter the numeric value');
+if(isNaN(intnumber)== false){
+    switch (intnumber){
+        case "0":
+        console.log("orez");
+        break;
+
+        case "1":
+        console.log("eno");
+        break;
+
+        case "2":
+        console.log("owt");
+        break;
+
+        case "3":
+        console.log("eerht");
+        break;
+
+        case "4":
+        console.log("ruof");
+        break;
+
+        case "5":
+        console.log("evif");
+        break;
+
+        case "6":
+        console.log("xis");
+        break;
+
+        case "7":
+        console.log("neves");
+        break;
+
+        case "8":
+        console.log("thgie");
+        break;
+
+        case "9":
+        console.log("enin");
+        break;
+
+        case "10":
+        console.log("net");
+        break;
+
+        default:
+
+        console.log("Enter the Greater than 10 numbers");
+
+    }
+}
+else{
+    console.log("Enter the positive number");
+}
