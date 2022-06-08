d9bf60534d7f40bb3a0de8df31b2a4b7c573a475
Karuppasamych <106671462+Karuppasamych@users.noreply.github.com>
2022-06-08 13:15:37 +0530

    Code Updated

--- number-to-words.js
+++ number-to-words.js
@@ -1,55 +1,47 @@
+// Write A Program To Accept A Number From User And Print That Number In Words But In Reverse Order
+
+
 let intnumber = prompt('Enter the numeric value');
 if(isNaN(intnumber)== false){
-    switch (intnumber){
-        case "0":
-        console.log("orez");
-        break;
+    while( intnumber > 0){
 
-        case "1":
-        console.log("eno");
-        break;
-
-        case "2":
-        console.log("owt");
-        break;
-
-        case "3":
-        console.log("eerht");
-        break;
-
-        case "4":
-        console.log("ruof");
-        break;
-
-        case "5":
-        console.log("evif");
-        break;
-
-        case "6":
-        console.log("xis");
-        break;
-
-        case "7":
-        console.log("neves");
-        break;
-
-        case "8":
-        console.log("thgie");
-        break;
-
-        case "9":
-        console.log("enin");
-        break;
-
-        case "10":
-        console.log("net");
-        break;
-
-        default:
-
-        console.log("Enter the Greater than 10 numbers");
-
+        let rem = intnumber % 10;
+        let intnumberdiv = Math.floor(intnumber / 10);
+        intnumber = intnumberdiv;
+        switch(rem){
+            case 0 :
+                console.log ('Zero');
+                break;
+            case 1 :
+                console.log ('One');
+                break;
+            case 2 :
+                console.log ('Two');
+                break;
+            case 3 :
+                console.log ('Three');
+                break;
+            case 4 :
+                console.log ('Four');
+                break;
+            case 5 :
+                console.log( 'Five');
+                break;
+            case 6 :
+                console.log( 'Six');
+                break;    
+            case 7 :
+                console.log ('Seven');
+                break;    
+            case 8 :
+                console.log ('Eight');
+                break;
+            case 9 :
+                console.log ('Nine');
+                break;
+        }
     }
+    
 }
 else{
     console.log("Enter the positive number");
